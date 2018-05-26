38ba7df4bff282062082fa60bd06a5cd38abd42a
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-26 10:28:51 +1000

    got some new modules and started working on logins

--- Collector_V3/Collector_V3/__init__.py
+++ Collector_V3/Collector_V3/__init__.py
@@ -3,6 +3,10 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_login import login_manager
+
 app = Flask(__name__)
+login_manager(app)
+
 
 import Collector_V3.views
--- Collector_V3/Collector_V3/views.py
+++ Collector_V3/Collector_V3/views.py
@@ -3,7 +3,8 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
 from Collector_V3 import app
 
 @app.route('/')
@@ -15,6 +16,23 @@ def home():
         title='Home Page',
         year=datetime.now().year,
     )
+
+@app.route('/login')
+def login():
+    return render_template(
+        'login.html',
+        title='Login'
+        )
+
+def register():
+    return render_template(
+        'regester.html',
+        title='Register'
+        )
+
+def logout():
+    flash('You have been logged out!')
+    return redirect(url_for('home'))
 
 @app.route('/contact')
 def contact():
